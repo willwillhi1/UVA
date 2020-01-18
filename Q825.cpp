24201db1da2b2e08bb074aef676f4704a80dcc5d
willwillhi1 <45191306+willwillhi1@users.noreply.github.com>
2020-01-18 16:54:22 +0800

    Create Q825.cpp

---
+++ Q825.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <sstream>
+
+using namespace std;
+
+int main(){
+	int T,W,N;
+	cin >> T;
+	while(T--){
+		cin >> W >> N;
+		bool map[100][100] = {0};
+		int dp[100][100] = {0};
+		cin.ignore();
+		string str;
+		for(int i=1,j;i <= W;i++){
+			getline(cin, str);
+			stringstream ss(str);
+			ss >> j;
+			while(ss >> j){
+				map[i][j] = true;
+			}
+		}
+		dp[1][1] = 1;
+		for(int i=1;i <= W;i++){
+			for(int j=1;j <= N;j++){
+				if(map[i][j] == true) continue;
+				dp[i][j] += dp[i][j-1] + dp[i-1][j];
+			}
+		}
+		/*
+		for(int i = 0;i <= W;i++){
+			for(int j = 0;j <= N;j++){
+				cout << dp[i][j] << " ";
+				
+			}
+			cout << endl;
+		}
+		*/
+		cout << dp[W][N] << endl;
+	}
+	return 0;
+}
