1a460ad87508699879973b03cbb666edf81fd20f
willwillhi1 <45191306+willwillhi1@users.noreply.github.com>
2020-03-19 10:06:31 +0800

    Create Q483.cpp

---
+++ Q483.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+#include <stack>
+#include <string>
+using namespace std;
+
+int main(){
+	string str;
+	vector<stack<char>> v;
+	while(cin >> str){
+		stack<char> s;
+		int size = str.size();
+		for(int i = 0;i < size;i++) s.push(str[i]);
+		v.push_back(s);
+		//char c = getchar();
+		//cout << str << endl;
+		if(s.top() == '.'){
+			//cout << "hi" << endl;
+			size = v.size();
+			for(int i = 0;i < size;i++){
+				while(!v[i].empty()){
+					cout << v[i].top();
+					v[i].pop();
+				}
+				if(i < size-1) cout << " ";
+				else{cout << endl;}
+			}
+			v.clear();
+		}
+	}
+	return 0;
+}
